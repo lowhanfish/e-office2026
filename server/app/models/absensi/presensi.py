f922cc7d8a9b6b2e8e4fc934d5eddc1b25b1e48e
lowhanfish <lowhanfish@gmail.com>
2026-04-07 14:20:22 +0800

    added AbsensiApel Model

--- server/app/models/absensi/presensi.py
+++ server/app/models/absensi/presensi.py
@@ -35,3 +35,30 @@ class Absensi(Base):
     createdAt = Column(DateTime(timezone=True), server_default=func.now())
 
 
+class AbsensiApel(Base):
+    __tablename__ = "absensi_apel"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    jenispresensi =Column(Integer, index=True, default=0, comment="diambil dari table presensi")
+    jenisKategori =Column(Integer, nullable=False, default=0)
+    jenisizin =Column(Integer, nullable=False, default=0)
+    jadwalapel =Column(Integer, nullable=False, default=0, comment="di ambil dari table jadwalapel")
+    lat = Column(Double, nullable=False)
+    lng = Column(Double, nullable=False)
+    jamDatang = Column(String(10), nullable=False, index=True)
+    dd = Column(SmallInteger, nullable=False, index=True)
+    mm = Column(SmallInteger, nullable=False, index=True)
+    yy = Column(SmallInteger, nullable=False, index=True)
+    keterangan =Column(Text, nullable=True)
+    NIP = Column(String(35), nullable=False, index=True)
+    status = Column(Boolean, default=False, index=True, comment="0=proses, 1=disetujui")
+    unit_kerja = Column(String(35), nullable=False,index=True)
+    fileRef = Column(String(35), nullable=True)
+    createdBy = Column(String(35), nullable=False)
+    createdAt = Column(DateTime(timezone=True), server_default=func.now())
+
+    __table_args__ = (
+        {"schema" : "absensi"},
+        UniqueConstraint("NIP", "dd", "mm", "yy", name="absensi_apel_uniq_tgl"),
+        CheckConstraint("dd BETWEEN 1 AND 31" , name="absensi_apel_dd"),
+        CheckConstraint("mm BETWEEN 1 AND 12" , name="absensi_apel_mm")
+    )
