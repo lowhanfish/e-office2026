f64682b61c11e106f2203de41a72610e2137a068
lowhanfish <lowhanfish@gmail.com>
2026-05-21 23:04:43 +0800

    added router1

--- server/app/schemas/simpeg/master/ref_kel_jabatan.py
+++ server/app/schemas/simpeg/master/ref_kel_jabatan.py
@@ -1,2 +1,25 @@
 from app.schemas.simpeg.master.base_schemas import MasterBase, MasterResponse, MasterCreate
 
+
+
+class KelJabatanResponse():
+    pass
+
+class KelJabatanCreate():
+    pass
+
+class KelJabatanUpdate():
+    pass
+
+
+
+# class JenisJabatanResponse(MasterResponse):
+#     pass
+
+# class JenisJabatanCreate(MasterCreate):
+#     pass
+
+# class JenisJabatanUpdate(MasterBase):
+#     id : Optional[str] = None
+#     kode : Optional[str] = None
+#     nama : Optional[str] = None
