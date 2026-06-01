9aeca1e6caf89d2da48283191ed60f9c8452d2b4
lowhanfish <lowhanfish@gmail.com>
2026-06-01 17:41:45 +0800

    added comment ref Instansi

--- server/app/api/v1/endpoints/simpeg/master/ref_instansi.py
+++ server/app/api/v1/endpoints/simpeg/master/ref_instansi.py
@@ -12,12 +12,42 @@ router = APIRouter()
 
 @router.get("/read", response_model=List[InstansiResponse])
 async def read_Instansi(db:AsyncSession = Depends(get_db)):
+
+    """
+    ## Mengambil semua List Instansi
+    Membaca data Instansi baru dari sistem.
+
+    **Parameter:**
+    - `search`   : String, Untuk mencari data value dari Instansi.
+    - `page_start`: Int, Data page pertama akses page.
+    - `page_limit` : Int, Jumlah data yang ditarik.
+
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
+
     query = select(Instansi)
     result = await db.execute(query)
     return result.scalars().all()
 
 @router.post("/create", response_model=InstansiResponse)
 async def create_Instansi(payload : InstansiCreate, db:AsyncSession = Depends(get_db)):
+    
+    """
+    ## Membuat Ref Instansi
+    Menambahkan data Instansi baru ke dalam sistem.
+
+    **Parameter:**
+    - `kode`: **String**, harus unik (sebaiknya di ambil dari `id` tabel referensi BKN).
+    - `nama`: **String**, Nama Instansi.
+    - `kode_cepat`: **String**, harus unik (sebaiknya di ambil dari `kode_cepat` tabel referensi BKN).
+    - `jenis`: **String**, Jenis instansi (**P** : Pusat, **D** : Daerah).
+    - `jenis_instansi_id`: **String**, jenis instansi (**KO**: Kementerian Koordinator, **KEMENT**: Kementerian, **LPNK**: Lembaga non Kementerian, **LNS**: Lembaga non Struktural, **PROV**: Provinsi, **KAB**: Kabupaten, **KOTA**: Kota).
+
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
+    
     query = Instansi(
         kode = payload.kode,
         kode_cepat = payload.kode_cepat,
@@ -35,6 +65,27 @@ async def create_Instansi(payload : InstansiCreate, db:AsyncSession = Depends(ge
 
 @router.put("/update/{id}", response_model=InstansiResponse)
 async def update_Instansi(id:str, payload: InstansiUpdate, db: AsyncSession = Depends(get_db)):
+    
+    """
+    ## Mengubah Instansi
+    Mengubah data item Instansi di dalam sistem.
+
+    **Key Path:**
+    - `id`: **String**, Di ambil dari `id` data item yang akan kita ubah.
+
+    **Parameter:**
+    *(Parameter dapat dihapus jika tidak diperlukan)*
+    - `kode`: **String**, harus unik (sebaiknya di ambil dari `id` tabel referensi BKN).
+    - `nama`: **String**, Nama Instansi.
+    - `kode_cepat`: **String**, harus unik (sebaiknya di ambil dari `kode_cepat` tabel referensi BKN).
+    - `jenis`: **String**, Jenis instansi (**P** : Pusat, **D** : Daerah).
+    - `jenis_instansi_id`: **String**, jenis instansi (**KO**: Kementerian Koordinator, **KEMENT**: Kementerian, **LPNK**: Lembaga non Kementerian, **LNS**: Lembaga non Struktural, **PROV**: Provinsi, **KAB**: Kabupaten, **KOTA**: Kota).
+
+    
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
+    
     query = select(Instansi).filter(Instansi.id == id)
     result = await db.execute(query)
     db_data = result.scalar_one_or_none()
@@ -55,6 +106,18 @@ async def update_Instansi(id:str, payload: InstansiUpdate, db: AsyncSession = De
 
 @router.delete("/delete/{id}")
 async def delete_Instansi(id:str, db: AsyncSession = Depends(get_db)):
+
+    """
+    ## Menghapus Instansi
+    Menghapus data item Instansi di dalam sistem.
+
+    **Key Path:**
+    - `id`: **String**, Di ambil dari `id` data item yang akan kita ubah.
+
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
+
     query = select(Instansi).filter(Instansi.id == id)
     result = await db.execute(query)
     db_data = result.scalar_one_or_none()
