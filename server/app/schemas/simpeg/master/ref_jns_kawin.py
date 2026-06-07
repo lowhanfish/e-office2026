dfe3d83deddeca808cead28cf4db94ba8da18ef4
lowhanfish <lowhanfish@gmail.com>
2026-06-07 21:39:35 +0800

    added endpoint ref jenis kawin

---
+++ server/app/schemas/simpeg/master/ref_jns_kawin.py
@@ -0,0 +1,12 @@
+from app.schemas.simpeg.master.base_schemas import MasterCreate, MasterResponse, MasterUpdate
+
+
+
+class RefJnsKawinResponse(MasterResponse):
+    pass
+
+class RefJnsKawinCreate(MasterCreate):
+    pass
+
+class RefJnsKawinUpdate(MasterUpdate):
+    pass
