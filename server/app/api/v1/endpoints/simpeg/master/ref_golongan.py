810d8af0b0dd4916cb1e0fc04b6a56af86c077cb
lowhanfish <lowhanfish@gmail.com>
2026-06-07 15:20:07 +0800

    added comments ref golongan

--- server/app/api/v1/endpoints/simpeg/master/ref_golongan.py
+++ server/app/api/v1/endpoints/simpeg/master/ref_golongan.py
@@ -10,6 +10,18 @@ router = APIRouter()
 
 @router.get("/read", response_model=List[RefGolonganResponse])
 async def read_ref_golongan(db:AsyncSession = Depends(get_db)):
+    """
+    ## Mengambil semua List Ref Golongan
+    Membaca data Ref Golongan baru dari sistem.
+
+    **Parameter:**
+    - `search`   : String, Untuk mencari data value dari Ref Golongan.
+    - `page_start`: Int, Data page pertama akses page.
+    - `page_limit` : Int, Jumlah data yang ditarik.
+
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
     query = select(RefGolongan)
     result = await db.execute(query)
     return result.scalars().all()
@@ -17,6 +29,19 @@ async def read_ref_golongan(db:AsyncSession = Depends(get_db)):
 
 @router.post("/create", response_model=RefGolonganResponse)
 async def create_ref_golongan(payload:RefGolonganCreate, db:AsyncSession = Depends(get_db)):
+    """
+    ## Membuat Ref Golongan
+    Menambahkan data Ref Golongan baru ke dalam sistem.
+
+    **Parameter:**
+    - `kode`: **String**, harus unik (sebaiknya di ambil dari `id` tabel referensi BKN).
+    - `nama`: **String**, Nama Ref Golongan (ex : III/b).
+    - `nama_pangkat`: **String**, Nama Pangkat dari ref golongan (ex: Penata Muda Tingkat I).
+    - `gol_pppk`: **String**, Pangkat Ref Golongan PPPK yang setara (ex: X).
+    
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
     new_data = RefGolongan(
         kode = payload.kode,
         nama = payload.nama,
@@ -24,14 +49,31 @@ async def create_ref_golongan(payload:RefGolonganCreate, db:AsyncSession = Depen
         gol_pppk = payload.gol_pppk,
         created_by = "user.id",
     )
-
     db.add(new_data)
     await db.commit()
     await db.refresh(new_data)
     return new_data
 
+
 @router.put("/update/{id}", response_model=RefGolonganResponse)
 async def update_ref_golongan(id:str, payload:RefGolonganUpdate, db:AsyncSession = Depends(get_db)):
+    """
+    ## Mengubah Ref Golongan
+    Mengubah data item Ref Golongan di dalam sistem.
+
+    **Key Path:**
+    - `id`: **String**, Di ambil dari `id` data item yang akan kita ubah.
+
+    **Parameter:**
+    *(Parameter dapat dihapus jika tidak diperlukan)*
+     - `kode`: **String**, harus unik (sebaiknya di ambil dari `id` tabel referensi BKN).
+    - `nama`: **String**, Nama Ref Golongan (ex : III/b).
+    - `nama_pangkat`: **String**, Nama Pangkat dari ref golongan (ex: Penata Muda Tingkat I).
+    - `gol_pppk`: **String**, Pangkat Ref Golongan PPPK yang setara (ex: X).
+   
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
     query = select(RefGolongan).where(RefGolongan.id == id)
     result = await db.execute(query)
     db_data = result.scalar_one_or_none()
@@ -40,7 +82,6 @@ async def update_ref_golongan(id:str, payload:RefGolonganUpdate, db:AsyncSession
         raise HTTPException(status_code=404, detail="Id dari data yang anda pilih tidak ditemukan")
     
     update_data = payload.model_dump(exclude_unset=True)
-
     for key, value in update_data.items():
         if hasattr(db_data, key):
             setattr(db_data, key, value)
@@ -52,6 +93,16 @@ async def update_ref_golongan(id:str, payload:RefGolonganUpdate, db:AsyncSession
 
 @router.delete("/delete/{id}")
 async def delete_ref_golongan(id:str, db:AsyncSession = Depends(get_db)):
+    """
+    ## Menghapus Ref Golongan
+    Menghapus data item Ref Golongan di dalam sistem.
+
+    **Key Path:**
+    - `id`: **String**, Di ambil dari `id` data item yang akan kita ubah.
+
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
     query = select(RefGolongan).where(RefGolongan.id == id)
     result = await db.execute(query)
     db_data = result.scalar_one_or_none()
@@ -60,7 +111,6 @@ async def delete_ref_golongan(id:str, db:AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Id dari data yang anda pilih tidak ditemukan")
     
     nama = db_data.nama
-
     await db.delete(db_data)
     await db.commit()
     return {"message": f"Ref Golongan {nama} berhasil dihapus"}
