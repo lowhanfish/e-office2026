362bcb13494119390a49530ef4b12b4482d92efb
lowhanfish <lowhanfish@gmail.com>
2026-06-07 22:32:49 +0800

    added schema Ref Jns Lokasi

---
+++ server/app/schemas/simpeg/master/ref_jns_lokasi.py
@@ -0,0 +1,11 @@
+from app.schemas.simpeg.master.base_schemas import MasterCreate, MasterResponse, MasterUpdate
+
+
+class RefJnsLokasiResponse(MasterResponse):
+    pass
+
+class RefJnsLokasiCreate(MasterCreate):
+    pass
+
+class RefJnsLokasiUpdate(MasterUpdate):
+    pass
