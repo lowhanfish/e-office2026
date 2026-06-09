2f65c078c1802b22047a74989857e6552f136c7d
lowhanfish <lowhanfish@gmail.com>
2026-06-09 22:00:09 +0800

    added commets ref jns lokasi

--- server/app/api/v1/endpoints/simpeg/master/ref_jns_lokasi.py
+++ server/app/api/v1/endpoints/simpeg/master/ref_jns_lokasi.py
@@ -13,12 +13,35 @@ router = APIRouter()
 
 @router.get("/read", response_model=List[RefJnsLokasiResponse])
 async def read_ref_jns_lokasi(db:AsyncSession = Depends(get_db)):
+    """
+    ## Mengambil semua List Ref Jenis Lokasi
+    Membaca data Ref Jenis Lokasi baru dari sistem.
+
+    **Parameter:**
+    - `search`   : String, Untuk mencari data value dari Ref Jenis Lokasi.
+    - `page_start`: Int, Data page pertama akses page.
+    - `page_limit` : Int, Jumlah data yang ditarik.
+
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
     query = select(RefJnsLokasi)
     result = await db.execute(query)
     return result.scalars().all()
 
 @router.post("/create", response_model=RefJnsLokasiResponse)
 async def create_ref_jns_lokasi(payload:RefJnsLokasiCreate, db:AsyncSession = Depends(get_db)):
+    """
+    ## Membuat Ref Jenis Lokasi
+    Menambahkan data Ref Jenis Lokasi baru ke dalam sistem.
+
+    **Parameter:**
+    - `kode`: **String**, harus unik (sebaiknya di ambil dari `id` tabel referensi BKN).
+    - `nama`: **String**, Nama Ref Jenis Lokasi.
+
+    **Error yang mungkin terjadi:**
+    - `422`: Jika format input tidak sesuai skema.
+    """
     new_data = RefJnsLokasi(
         kode = payload.kode,
         nama = payload.nama,
