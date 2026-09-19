fd6c84c55b028c68062405df8610285c89deaf36
lowhanfish <lowhanfish@gmail.com>
2026-09-19 23:29:52 +0800

    added RefLevelKompetensiAsn schema

--- server/app/api/v1/endpoints/simpeg/master/ref_level_kompetensi_asn.py
+++ server/app/api/v1/endpoints/simpeg/master/ref_level_kompetensi_asn.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.models.simpeg.master.models import RefLevelKompetensiAsn, User
+from app.api.deps import get_current_user
+from app.db.transaction import commit_or_raise_unique_conflict
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.sql import select
 
 
 router = APIRouter()
---
+++ server/app/schemas/simpeg/master/ref_level_kompetensi_asn.py
@@ -0,0 +1,11 @@
+from app.schemas.simpeg.master.base_schemas import MasterResponseList, MasterResponse, MasterCreate
+
+
+class RefLevelKompetensiAsnCreate(MasterCreate):
+    pass
+
+class RefLevelKompetensiAsnResponse(MasterResponse):
+    pass
+
+class RefLevelKompetensiAsnResponseList(MasterResponseList):
+    pass
